ce878e1d0ddbd76f996f1ffc7b43f7834925edc1
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 17:55:31 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import Header from '@/components/layout/Header';
+import PostCard from '@/components/common/PostCard';
+import { PostProps } from '@/interfaces';
 
-const PostsPage: React.FC = () => {
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const PostsPage: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div>
       <Header />
       <h1>Posts Page</h1>
-      <p>Here you can display posts dynamically.</p>
+      {posts.map((post) => (
+        <PostCard
+          key={post.userId + post.title}
+          title={post.title}
+          content={post.content}
+          userId={post.userId}
+        />
+      ))}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const data = await res.json();
+
+  // Map API fields to our PostProps
+  const posts: PostProps[] = data.map((post: any) => ({
+    userId: post.userId,
+    title: post.title,
+    content: post.body,
+  }));
+
+  return {
+    props: { posts },
+  };
 };
 
 export default PostsPage;
