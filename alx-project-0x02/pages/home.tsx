202efd25d9a563ddb89595401dafdd35a67310f4
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 17:41:55 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '@/components/layout/Header';
 import Card from '@/components/common/Card';
+import { CardProps } from '@/interfaces';
 
 const HomePage: React.FC = () => {
   return (
     <div>
+      <Header />
       <h1>Welcome to Home Page</h1>
-
       <Card title="Card 1" content="This is the first card." />
       <Card title="Card 2" content="This is the second card." />
       <Card title="Card 3" content="This is the third card." />
