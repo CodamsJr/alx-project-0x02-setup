ba0cceaf26cb2b50b58a40a32b4834b4692efaac
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 15:08:31 +0100

    Update home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,12 @@
+// pages/home.tsx
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of your Next.js project.</p>
+    </div>
+  );
+}
