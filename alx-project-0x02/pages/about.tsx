e5ac46586f4726959f63438864bdb90d9ed7151d
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 15:09:03 +0100

    Update about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,12 @@
+// pages/about.tsx
+import Header from '../components/Header';
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <h1>About Us</h1>
+      <p>This is the about page of your Next.js project.</p>
+    </div>
+  );
+}
