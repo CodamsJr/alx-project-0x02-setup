2d04ab006aa13f1a334cad969675148fc20e4063
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 15:07:57 +0100

    Update Header.tsx

---
+++ alx-project-0x02/components/Header.tsx
@@ -0,0 +1,12 @@
+// components/Header.tsx
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link href="/home">Home</Link> | <Link href="/about">About</Link>
+      </nav>
+    </header>
+  );
+}
