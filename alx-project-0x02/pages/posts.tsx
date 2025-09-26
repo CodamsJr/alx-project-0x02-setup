1bb8fea8bee8c8f72951706f675a2ea1c29e4f47
CodamsJr <oguntolahammed076@gmail.com>
2025-09-26 17:32:50 +0100

    Create posts.tsx

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/components/layout/Header';
+
+const PostsPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Posts Page</h1>
+      <p>Here you can display posts dynamically.</p>
+    </div>
+  );
+};
+
+export default PostsPage;
